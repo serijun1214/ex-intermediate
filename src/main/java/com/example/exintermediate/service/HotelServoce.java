f51e715078cc8a935c2fad6eedf60fc67df61edf
serijun1214 <junki.seri@rakus-partners.co.jp>
2024-02-13 04:00:09 +0900

    ホテルのサービスクラスを作成

---
+++ src/main/java/com/example/exintermediate/service/HotelServoce.java
@@ -0,0 +1,29 @@
+package com.example.exintermediate.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.exintermediate.domain.Hotel;
+import com.example.exintermediate.repository.HotelRepository;
+
+/**
+ * hotelsテーブルを操作するサービス
+ */
+@Service
+public class HotelServoce {
+  
+  // HotelRepositoryをDI
+  @Autowired
+  private HotelRepository hotelRepository;
+
+  /**
+   * 全てのホテル情報を取得
+   * 
+   * @return ホテル情報のリスト
+   */
+  public List<Hotel> findByPrice(Integer price) {
+    return hotelRepository.findByPrice(price);
+  }
+}
