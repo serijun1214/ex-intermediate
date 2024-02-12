e3fa9161dcae72c9c3112a7446f8e84ac454e556
serijun1214 <junki.seri@rakus-partners.co.jp>
2024-02-13 05:00:02 +0900

    ホテルコントローラーを修正

--- src/main/java/com/example/exintermediate/controller/HotelController.java
+++ src/main/java/com/example/exintermediate/controller/HotelController.java
@@ -1,14 +1,16 @@
 package com.example.exintermediate.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.exintermediate.domain.Hotel;
 import com.example.exintermediate.service.HotelService;
-
-
 
 @Controller
 @RequestMapping("")
@@ -17,14 +19,21 @@ public class HotelController {
   @Autowired
   private HotelService hotelService;
 
-  @GetMapping("/showHotelList")
-  public String showHotelList() {
-    return "ex02/hotel-search";
-  }
-
   @GetMapping("/hotelSearch")
-  public String hotelSearch(Integer price, Model model){
-    model.addAttribute("hotelList", hotelService.findByPrice(price));
+  public String hotelSearch(String price, Model model) {
+
+    List<Hotel> hotelList = null;
+
+    if (price == null) {
+      return "ex02/hotel-search";
+    }
+    if (price.isEmpty()) {
+      hotelList = hotelService.findByPrice(null); // 全件検索
+    } else {
+      hotelList = hotelService.findByPrice(Integer.parseInt(price));
+    }
+
+    model.addAttribute("hotelList", hotelList);
     model.addAttribute("price", price);
     return "ex02/hotel-search";
   }
